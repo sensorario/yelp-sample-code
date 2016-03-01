bc57e7615b0f60d367a177f7f5d54daf42160e4f
Simone Gentili <sensorario@gmail.com>
2016-03-01 09:30:40 +0100

    Complete Sherlock coverage

--- src/Sensorario/Yelp/Tests/SherlockTest.php
+++ src/Sensorario/Yelp/Tests/SherlockTest.php
@@ -10,16 +10,26 @@ use Sensorario\Yelp\Sherlock;
 
 final class SherlockTest extends PHPUnit_Framework_TestCase
 {
-    public function testGenericSearch()
+    /** @dataProvider searches */
+    public function testGenericSearch($search)
     {
         $httpClient = $this->getMockBuilder('Sensorario\Yelp\HttpClient')
-            ->setMethods(['search'])
+            ->setMethods([$search])
             ->getMock();
         
         $service = new Sherlock(
             $httpClient
         );
 
-        $service->find('search');
+        $service->find($search);
+    }
+
+    public function searches()
+    {
+        return [
+            ['search'],
+            ['phone_search'],
+            ['business'],
+        ];
     }
 }
